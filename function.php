49be82df9714fb066822c6f3dcc8f286d3923f37
Aleksey <matokhin_aleksey@mail.ru>
2021-09-06 23:06:50 +0300

    main

--- function.php
+++ function.php
@@ -2,25 +2,14 @@
 
 function Products($count) {
     $mysqli = new Mysqli('localhost', 'root', '', 'test');
-
-
-
     $query = $mysqli->query('SELECT * FROM `Products` ORDER BY `DATE_CREATE` DESC');
-    $items = [];
-    while(($row = $query->fetch_assoc())) {
-        $items[] = $row;
-    }
-
     $items_result = [];
-    $query = $mysqli->query('SELECT * FROM `Products` ORDER BY `DATE_CREATE` DESC');
-    for($i = 1;($items = $query->fetch_assoc()); $i++) {
-
-        $items_result[] = $items;
+    for($i = 0;($items = $query->fetch_assoc()); $i++) {
         if($i >= $count) {
             break;
         }
+        $items_result[] = $items;
     }
-
     return $items_result;
 }
 
